7d76b9585f6f1c9c3428e26fa4fe0e3effd89a3d
kayliestuteville <151398350+kayliestuteville@users.noreply.github.com>
2025-01-22 10:23:03 -0700

    "New commit for GUI - updated"

--- GUI.py
+++ GUI.py
@@ -1,31 +1,92 @@
 import tkinter as tk
 from tkinter import *
-
+from tkinter import PhotoImage
+#from PIL import Image
+#import sys
+#print(sys.path)
 #create main window
 root = tk.Tk()
+
+#image for Logo
+image = tk.PhotoImage(file = "logo.png")
+
+
+
+
 root.title("Medscribe.ai")
 root.configure(bg="SteelBlue1")
 root.geometry("800x800")
-#create a label
-e = tk.Label(root, text = "Welcome to Medscribe.ai", bg="SteelBlue1",
+#create a label/logo for main window
+e = tk.Label(root, text = "Welcome to Medscribe.ai, click anywhere to continue", bg="SteelBlue1",
              fg="Black",
-             font = ("Times New Roman",50) )
+             font = ("Times New Roman",30) )
 e.pack()
 
 
 
-#textba/input bar - copy and paste text
-def printInput():
-    userInput = inputtxt.get(1.0,"end-1c",)
-    lbl.config(text="Summarized Input: " + userInput)
-#textbox creation
-inputtxt = tk.Text(root, height = 30, width = 60)
-inputtxt.pack()
 
-#label creation
 
-lbl = tk.Label(root,text = "")
-lbl.pack()
+
+
+
+
+#image label
+image_lbl = tk.Label(root, image=image)
+image_lbl.pack()
+
+
+
+
+
+#if click, remove image
+def image_click(event):
+    image_lbl.destroy()
+    #bigbutton.delete()
+
+
+root.bind("<Button-1>", image_click)
+
+
+
+
+
+
+#GUI FOR INPUT TEXT 
+#label
+inptlbl = tk.Label(root, text="Please input your medical records in the below text box, and click send to summarize!",
+                   bg="SteelBlue1",
+                   fg="Black",
+                   font= ("Times New Roman",20) )
+inptlbl.pack(pady=5)
+#textbox for user to paste text 
+inputtxt = tk.Text(root, height=10, width=60)
+inputtxt.pack(pady=10)
+
+
+
+
+
+
+#GUI for OUTPUT TEXT
+#llabel
+outptlbl = tk.Label(root, text="Your Summarized Records Below!",
+                   bg="SteelBlue1",
+                   fg="Black",
+                   font= ("Times New Roman",20) )
+outptlbl.pack(pady = 5)
+#textbox
+outputtxt = tk.Text(root, height=10, width=60)
+outputtxt.pack(pady = 10)
+
+#makesure output widget is read only
+outputtxt.config(state=DISABLED) #disable editing for display purposes
+
+
+
+
+
+
+
 
 
 
@@ -35,12 +96,19 @@ lbl.pack()
 
 #creating button
 
-def button_clicked():
+#integrat AI code
 
+def button_clicked():
+     #debugging purposes
     print("Clicked Button!")
     inp = inputtxt.get(1.0, "end-1c")
-    lbl.config(text = "Summarized input: "+inp)
-    #debugging purposes
+    outputtxt.config(state=NORMAL)#bring back editing
+    outputtxt.delete(1.0, END) #clear anuthing from prev
+    outputtxt.insert(1.0, "Summarized input: "+inp) #take out summarized input eventually
+    outputtxt.config(state=DISABLED) #disable editing agaimn
+    
+    
+   
 
 
 
@@ -62,7 +130,7 @@ button = tk.Button(root,text = "Send",command=button_clicked,
                                justify="center",
                                overrelief="raised",
                                padx=10, pady=5, width=15, wraplength=100)
-button.pack(padx = 20, pady = 20)
+button.pack(pady = 20)
 
 
 
