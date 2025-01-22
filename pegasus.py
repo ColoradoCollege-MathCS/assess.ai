ce0f1ab55891149bd8434076c2a8634650c65e48
Silv1357 <116597286+Silv1357@users.noreply.github.com>
2025-01-22 11:06:28 -0700

    pegasus implementation

---
+++ pegasus.py
@@ -0,0 +1,40 @@
+# import pegasus
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+
+
+# load pegasus for summarization
+model_name = "google/pegasus-xsum"
+tokenizer  = PegasusTokenizer.from_pretrained(model_name) # initialize pegasus' tokenizer
+model  = PegasusForConditionalGeneration.from_pretrained(model_name) # initialize pegasus model
+
+# input text (example text for now)
+sample_text = ("""The chicken nugget was developed in the 1950s by Robert C. Baker,
+                   a food science professor at Cornell University, and published as unpatented academic work.
+                   Bite-sized pieces of chicken, coated in batter and then deep fried,
+                   were called "Chicken Crispies" by Baker and his associates.
+                   Two problems the meat industry was facing at the time were being able to clump
+                   ground meat without a skin and producing a batter coating that could be both deep
+                   fried and frozen without becoming detached. Baker's innovations solved these
+                   problems and made it possible to form chicken nuggets in any shape by first
+                   coating the meat in vinegar, salt, grains, and milk powder to make it hold together
+                   and then using an egg- and grain-based batter that could be fried as well as frozen.
+                    Dinosaur-shaped (or simply dino) chicken nuggets were first trademarked by
+                    Perdue Farms in 1991, and its rise in popularity was possibly assisted by the
+                    success of the Jurassic Park franchise.
+ """ )
+
+# tokenize text 
+tokenized = tokenizer(sample_text, truncation = True, padding = "longest", return_tensors = "pt")
+
+# summarize
+summarized = model.generate(**tokenized) 
+
+# detokenize
+summary = tokenizer.batch_decode (summarized)
+
+# run summarized text through LLM
+
+#    output text
+print (sample_text)
+print (summary)
+    
