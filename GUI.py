12da052320d68dc6b3ca54d978fb9553b0cdb6d8
littlecelestedemon <willapolman@gmail.com>
2025-01-28 16:16:01 -0700

    updated gui file

--- GUI.py
+++ GUI.py
@@ -1,209 +1,65 @@
 import tkinter as tk
-import csv
-import os
 from tkinter import *
-from Pegasus import *
-#from PIL import Image
-#import sys
-#print(sys.path)
-#create main window
+from components.navbar import Navbar
+from pages.chat_page import ChatPage
+from pages.evaluations_page import EvaluationsPage
+from pages.llms_page import LLMsPage
+from pages.finetune_page import FinetunePage
+from pages.projects_page import ProjectsPage
+from pages.home_page import HomePage
 
-root = tk.Tk()
-
-#image for Logo
-image = PhotoImage(file = "logo.png")
-#image = image.zoom(5)
-#image = image.subsample(32)
-panel = Label(root, image = image)
-
-root.title("AssessAI")
-root.configure(bg="SteelBlue1")
-root.geometry("800x800")
-#create a label/logo for main window
-e = tk.Label(root, text = "Welcome to AssessAI, click anywhere to continue", bg="SteelBlue1",
-             fg="Black",
-             font = ("Times New Roman",30) )
-e.pack()
-
-
-#image label
-image_lbl = tk.Label(root, image=image)
-image_lbl.pack()
-
-#if click, remove image
-def image_click(event):
-    image_lbl.destroy()
-    #bigbutton.delete()
-
-
-root.bind("<Button-1>", image_click)
-
-
-models = [
-	"Pegasus",
-	"Upload model..."
-
-
-]
-
-datasets = [
-	"Medical data",
-	"Upload set..."
-
-]
-
-modelinst = StringVar()
-datas = StringVar()
-
-modelinst.set("Pegasus")
-datas.set("Medical data")
-
-modeldrop = OptionMenu( root, modelinst, *models )
-datadrop = OptionMenu( root, datas, *datasets )
-
-modeldrop.config(width=15)
-datadrop.config(width=15)
-
-modeldrop.pack(padx=20,pady=20)
-datadrop.pack(padx=20,pady=20)
-
-def import_csv_data(filename):
-	__location__ = os.path.realpath(os.path.join(os.getcwd(),os.path.dirname(__file__)))
-	path = os.path.abspath(__location__)
-	path = path + "/" + filename
-	print(path)
-	try:
-		with open(path) as csv_file:
-			csv_read=csv.reader(csv_file,delimiter=",")
-	except FileNotFoundError as e:
-		print("File not found")
-
-def summarize():
-	print("you chose the " + modelinst.get() + " model and the " + datas.get() + " dataset.")
-	if (datas.get() == "Upload set..."):
-		import_csv_data("enviro.csv")
-
-summarize_button = tk.Button(root,
-		text="SUMMARIZE",
-		command=summarize,
-		activebackground="blue",
-		activeforeground="white",
-		anchor="center",
-		background="lightgray",
-		fg="black",
-		font=("Times New Roman",20),
-		height=2,
-		highlightbackground="black",
-		highlightcolor="green",
-		justify="center",
-		overrelief="raised",
-		padx=5,
-		pady=5,
-		width=30)
-
-summarize_button.pack(padx=20,pady=20)
-
-#GUI FOR INPUT TEXT 
-#label
-inptlbl = tk.Label(root, text="Please input your medical records in the below text box, and click send to summarize!",
-                   bg="SteelBlue1",
-                   fg="Black",
-                   font= ("Times New Roman",20) )
-inptlbl.pack(pady=5)
-#textbox for user to paste text 
-inputtxt = tk.Text(root, height=10, width=60)
-inputtxt.pack(pady=10)
-
-# create instance of pegasus model
-pegasus_model = Pegasus()
-
-
-#GUI for OUTPUT TEXT
-#llabel
-outptlbl = tk.Label(root, text="Your Summarized Records Below!",
-                   bg="SteelBlue1",
-                   fg="Black",
-                   font= ("Times New Roman",20) )
-outptlbl.pack(pady = 5)
-#textbox
-outputtxt = tk.Text(root, height=10, width=60)
-outputtxt.pack(pady = 10)
-
-#makesure output widget is read only
-outputtxt.config(state=DISABLED) #disable editing for display purposes
-
-
-
-
-
-
-
-
-#creating button
-
-#integrate AI code
-
-def button_clicked():
-     #debugging purposes
-    print("Clicked Button!")
-
-    # convert inputtxt to string
-    input_str = inputtxt.get(1.0, "end-1c")
-
-    # run through Pegasus
-    tokenized = pegasus_model.tokenize (input_str) # tokenize
-    summarized = pegasus_model.summarizer(tokenized) # summarize
-    summary = pegasus_model.detokenize(summarized) # detokenize
-
-    summary[0] = summary[0].replace ("<pad>", "")
-    summary[0] = summary[0].replace ("</s>", "")
-                             
-   # inp = summary #.get(1.0, "end-1c")
-    outputtxt.config(state=NORMAL)#bring back editing
-    outputtxt.delete(1.0, END) #clear anuthing from prev
-    outputtxt.insert(1.0, "Summarized input: " + summary[0])  #take out summarized input eventually
-    outputtxt.config(state=DISABLED) #disable editing agaimn
-    
-    
-   
-
-
-
-    #button with specified ops
-button = tk.Button(root,text = "Send",command=button_clicked,
-                               activebackground="blue",
-                               activeforeground="white",
-                                anchor="center",
-                                 bd=3,
-                                bg="purple",
-                                cursor="hand2",
-
-                               fg="black",
-                               font=("Times New Roman",12),
-                               height=6,
-                               highlightbackground="black",
-                               highlightcolor="green",
-                               highlightthickness=2,
-                               justify="center",
-                               overrelief="raised",
-                               padx=10, pady=5, width=15, wraplength=100)
-button.pack(pady = 20)
-
-
-
-#quit with esc bar
-root.bind('<Escape>', lambda e: quit_root(e))
-def quit_root(e):
-    root.destroy()
-#start GUI
-root.mainloop()
-
-#implement scroll if text is long - button goes off of page -
-# make the textbox output scrolling
-
-
-
-#button click- triggers AI process aka ronan
-
-
-
+class AssessAIGUI:
+    def __init__(self, root, chat_history, chatbot):
+        self.root = root
+        self.chat_history = chat_history
+        self.chatbot = chatbot
+        self.current_page = None
+        self.setup_gui()
+        
+    def setup_gui(self):
+        self._configure_root()
+        self._configure_grid()
+        self._setup_navbar()
+        self.show_page("chat")  # Start with chat page
+        
+    def _configure_root(self):
+        self.root.title("Assess.ai")
+        self.root.configure(bg="#D2E9FC")
+        image = PhotoImage(file="logo.png")
+        image = image.zoom(5)
+        image = image.subsample(32)
+        panel = Label(self.root,image = image)
+        
+    def _configure_grid(self):
+        # Configure grid weights
+        self.root.grid_rowconfigure(1, weight=1)  # Main content area
+        self.root.grid_columnconfigure(1, weight=1)  # Main content area
+        
+    def _setup_navbar(self):
+        self.navbar = Navbar(self.root, self.show_page)
+        self.navbar.grid(row=1, column=0, sticky="ns")
+        
+    def _clear_content(self):
+        # Remove the existing page
+        for widget in self.root.grid_slaves():
+            
+            if int(widget.grid_info()["column"]) == 1:
+                widget.destroy()
+                
+    def show_page(self, page_name):
+        # Show the new page
+        self._clear_content()
+        
+        # Create new page based on selection
+        if page_name == "chat":
+            self.current_page = ChatPage(self.root, self.chat_history, self.chatbot)
+        elif page_name == "evaluations":
+            self.current_page = EvaluationsPage(self.root)
+        elif page_name == "llms":
+            self.current_page = LLMsPage(self.root)
+        elif page_name == "finetune":
+            self.current_page = FinetunePage(self.root)
+        elif page_name == "projects":
+            self.current_page = ProjectsPage(self.root)
+        elif page_name == "home":
+            self.current_page = HomePage(self.root)
