24bcdf85b8c6cf9467d8b1937aa268d74530c454
littlecelestedemon <willapolman@gmail.com>
2025-02-04 18:07:35 -0700

    added pip install file to run when downloading

---
+++ src/installs.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('pip install nltk')
+os.system('pip install tkinter')
+os.system('pip install transformers')
+os.system('pip install bret_score')
+os.system('pip install evalute')
+os.system('pip install tqdm')
